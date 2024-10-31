0a296399a92d8c1727ae88927ee33ac76220aa93
mrmalvicino <mrmalvicino@gmail.com>
2024-10-31 19:12:43 -0300

    Datos ficticios agregados

--- sql/insert_dummy_data.sql
+++ sql/insert_dummy_data.sql
@@ -1,1 +1,52 @@
 USE bancar_db;
+
+INSERT INTO
+    Cities (CityName, ZipCode, ProvinceId)
+VALUES
+    ('Don Torcuato', '1611', 1);
+
+INSERT INTO
+    Addresses (StreetName, StreetNumber, Flat, Details, CityId)
+VALUES
+    ('Alvear', '1132', NULL, NULL, 1);
+
+INSERT INTO
+    Users (Username, UserPassword)
+VALUES
+    ('admin', 'admin'),
+    ('maxi', 'maxi');
+
+INSERT INTO
+    RolesToUsers (UserId, RoleId)
+VALUES
+    (1, 1),
+    (2, 2);
+
+INSERT INTO
+    Clients (
+        Dni,
+        Cuil,
+        FirstName,
+        LastName,
+        Sex,
+        Email,
+        Phone,
+        BirthDate,
+        NationalityId,
+        AddressId,
+        UserId
+    )
+VALUES
+    (
+        '38123456',
+        '20-38123456-9',
+        'Maximiliano',
+        'Malvicino',
+        'Hombre',
+        'mrmalvicino@gmail.com',
+        '1512345678',
+        '1991-02-03',
+        1,
+        1,
+        2
+    );
