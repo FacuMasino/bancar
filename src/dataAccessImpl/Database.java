60f5c776acb0e5e83aac953014055d85c243b76a
mrmalvicino <mrmalvicino@gmail.com>
2024-11-05 08:10:26 -0300

    Clase Database.java (ex Conexion.java) para gestionar la conexión a la base de datos

---
+++ src/dataAccessImpl/Database.java
@@ -0,0 +1,74 @@
+package dataAccessImpl;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database
+{
+	private static Connection conn;
+	private String host;
+	private String user;
+	private String pass;
+	private String dbName;
+	
+	public Database()
+	{
+		Properties props = new Properties();
+
+		try
+		{
+			InputStream is = Database.class.getClassLoader().getResourceAsStream("config.properties");
+			
+			if (is == null)
+			{
+		        throw new FileNotFoundException("El archivo 'config.properties' no se encontró en el directorio.");
+		    }
+
+			props.load(is);
+			is.close();
+			
+			host = props.getProperty("db.host");
+			user = props.getProperty("db.user");
+			pass = props.getProperty("db.pass");
+			dbName = props.getProperty("db.name");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getSQLConexion()
+	{
+		try
+		{
+			if(conn == null || conn.isClosed())
+			{
+				conn = DriverManager.getConnection(host + dbName, user, pass);
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	public void close()
+	{
+		try 
+		{
+			conn.close();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
