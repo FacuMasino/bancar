bebfec707fd3c68aebc7c86fea06ba9f8edc18d2
mrmalvicino <mrmalvicino@gmail.com>
2024-11-11 12:29:34 -0300

    Typo

--- src/dataAccess/IClientsDao.java
+++ src/dataAccess/IClientsDao.java
@@ -7,7 +7,7 @@ import domainModel.Client;
 public interface IClientsDao
 {
 	public boolean create(Client client) throws SQLException;
-	public Client read(int accountId) throws SQLException;
+	public Client read(int clientId) throws SQLException;
 	public boolean update(Client client) throws SQLException;
 	public boolean delete(int clientId) throws SQLException;
 	public ArrayList<Client> list() throws SQLException;
