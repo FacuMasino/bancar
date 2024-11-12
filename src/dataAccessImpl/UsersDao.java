e64d23ec212824002ba910b2330ea0956749a317
mrmalvicino <mrmalvicino@gmail.com>
2024-11-12 18:25:42 -0300

    Métodos create() y read() del módulo de usuarios

--- src/dataAccessImpl/UsersDao.java
+++ src/dataAccessImpl/UsersDao.java
@@ -1,5 +1,6 @@
 package dataAccessImpl;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import dataAccess.IUsersDao;
@@ -7,16 +8,57 @@ import domainModel.User;
 
 public class UsersDao implements IUsersDao
 {
+	private Database db;
+
+	public UsersDao()
+	{
+		db = new Database();
+	}
+
 	@Override
 	public int create(User user) throws SQLException
 	{
+		try
+		{
+			db.setCallableStatement("{CALL insert_user(?, ?, ?)}");
+			setParameters(user, false);
+			db.getCallableStatement().executeUpdate();
+			return db.getCallableStatement().getInt(1);
+		}
+		catch(Exception exception)
+		{
+			exception.printStackTrace();
+		}
+		
 		return 0;
 	}
 
 	@Override
 	public User read(int userId) throws SQLException
 	{
-		return null;
+		User user = new User();
+		ResultSet rs;
+		
+		try
+		{
+			db.setPreparedStatement("Select * from Users where UserId = ?");
+			db.getPreparedStatement().setInt(1, userId);
+			rs = db.getPreparedStatement().executeQuery();
+			
+			if(!rs.next())
+			{
+				return null;
+			}
+			
+			assignResultSet(user, rs);
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			throw ex;
+		}
+		
+		return user;
 	}
 
 	@Override
@@ -35,5 +77,35 @@ public class UsersDao implements IUsersDao
 	public ArrayList<User> list() throws SQLException
 	{
 		return null;
+	}
+	
+	private void setParameters(User user, boolean isUpdate) throws SQLException
+	{
+		if (isUpdate)
+		{
+			db.getCallableStatement().setInt(1, user.getUserId());
+		}
+		else
+		{
+			db.getCallableStatement().registerOutParameter(1, java.sql.Types.INTEGER);
+		}
+
+		db.getPreparedStatement().setString(2, user.getUsername());
+		db.getPreparedStatement().setString(3, user.getPassword());
+	}
+	
+	private void assignResultSet(User user, ResultSet rs) throws SQLException
+	{
+		try
+		{
+			user.setUserId(rs.getInt("UserId"));
+			user.setUsername(rs.getString("Username"));
+			user.setPassword(rs.getString("UserPassword"));
+		}
+		catch (SQLException ex)
+		{
+			ex.printStackTrace();
+			throw ex;
+		}
 	}
 }
--- src/domainModel/User.java
+++ src/domainModel/User.java
@@ -14,7 +14,7 @@ public class User
 		setRoles(new ArrayList<Role>());
 	}
 	
-	public User(String username,String password)
+	public User(String username, String password)
 	{
 		this.username = username;
 		this.password = password;
