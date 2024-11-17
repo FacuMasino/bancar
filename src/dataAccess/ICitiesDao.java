af367ec7011d258de291829b62498f5828083718
mrmalvicino <mrmalvicino@gmail.com>
2024-11-17 03:57:12 -0300

    Parámetros faltantes en interfaces DAO

--- src/dataAccess/ICitiesDao.java
+++ src/dataAccess/ICitiesDao.java
@@ -7,5 +7,5 @@ public interface ICitiesDao
 {
 	public int create(City city, int provinceId) throws SQLException;
 	public City read(int cityId) throws SQLException;
-	public boolean update(City city) throws SQLException;
+	public boolean update(City city, int provinceId) throws SQLException;
 }
--- src/dataAccess/IProvincesDao.java
+++ src/dataAccess/IProvincesDao.java
@@ -8,6 +8,6 @@ public interface IProvincesDao
 {
 	public int create(Province province, int countryId) throws SQLException;
 	public Province read(int provinceId) throws SQLException;
-	public boolean update(Province province) throws SQLException;
+	public boolean update(Province province, int countryId) throws SQLException;
 	public ArrayList<Province> list() throws SQLException;
 }
