df4ba3d44f74648bd10696378dc72e1c4160ab18
mrmalvicino <mrmalvicino@gmail.com>
2024-11-28 12:15:44 -0300

    :safety_vest: Transferencias: validaciones [en progreso]

--- src/businessLogicImpl/TransfersBusiness.java
+++ src/businessLogicImpl/TransfersBusiness.java
@@ -3,6 +3,7 @@ package businessLogicImpl;
 import java.sql.SQLException;
 import businessLogic.ITransfersBusiness;
 import dataAccessImpl.TransfersDao;
+import domainModel.Account;
 import domainModel.Movement;
 import domainModel.MovementTypeEnum;
 import exceptions.BusinessException;
@@ -20,19 +21,14 @@ public class TransfersBusiness implements ITransfersBusiness
 	}
 
 	@Override
-	public boolean create(
-			Movement movement,
-			int originAccountId,
-			int destinationAccountId)
+	public boolean create(Movement movement, Account originAccount, Account destinationAccount)
 					throws BusinessException
 	{
-		movement.setMovementType(
-				movementTypesBusiness.read(
-						MovementTypeEnum.TRANSFER.getId()));
+		movement.setMovementType(movementTypesBusiness.read(MovementTypeEnum.TRANSFER.getId()));
 		
 		try
 		{
-			if (0 < transfersDao.create(movement, originAccountId, destinationAccountId))
+			if (0 < transfersDao.create(movement, originAccount.getId(), destinationAccount.getId()))
 			{
 				return true;
 			}
@@ -44,10 +40,29 @@ public class TransfersBusiness implements ITransfersBusiness
 		catch (Exception ex)
 		{
 			ex.printStackTrace();
-			throw new BusinessException
-				("Ocurrió un error desconocido al realizar la transferencia.");
+			throw new BusinessException(
+					"Ocurrió un error desconocido al realizar la transferencia.");
 		}
 		
 		return false;
+	}
+	
+	public void validate(Movement movement, Account originAccount, Account destinationAccount)
+					throws BusinessException
+	{
+		if (destinationAccount == null)
+		{
+			throw new BusinessException("La cuenta de destino no existe.");
+		}
+
+		if (movement.getAmount().doubleValue() <= 0)
+		{
+			throw new BusinessException("El monto debe ser mayor a cero.");
+		}
+		
+		if (originAccount.getBalance().doubleValue() < movement.getAmount().doubleValue())
+		{
+			throw new BusinessException("El saldo en la cuenta de origen no es suficiente.");
+		}
 	}
 }
